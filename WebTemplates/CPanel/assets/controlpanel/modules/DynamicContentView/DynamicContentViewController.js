a1a7a0284cfb2bd2c9e77d069f764c9a57750530
Kalle Launiala <kalle.launiala@protonit.net>
2014-09-06 13:47:39 +0300

    Dynamic content skeleton, advanced editing structures done

--- WebTemplates/CPanel/assets/controlpanel/modules/DynamicContentView/DynamicContentViewController.js
+++ WebTemplates/CPanel/assets/controlpanel/modules/DynamicContentView/DynamicContentViewController.js
@@ -18,6 +18,8 @@ define(["require", "exports", "../ViewControllerBase"], function(require, export
             var me = this;
             require([
                 "DynamicContentView/DynamicContent_dust",
+                "DynamicContentView/DynamicContentAdvanced_dust",
+                "DynamicContentView/DynamicContentPageSkeleton_dust",
                 "DynamicContentView/DynamicContentView_Modals_dust",
                 "lib/dusts/objectdeleteicon_dust",
                 "lib/dusts/command_button_dust",
@@ -51,11 +53,46 @@ define(["require", "exports", "../ViewControllerBase"], function(require, export
             //alert("Connections view ctrl invisible render: " + this.divID);
         };
 
+        DynamicContentViewController.prototype.OpenAddDynamicContentGroupModal = function () {
+            var $modal = this.$getNamedFieldWithin("AddNewDynamicContentGroupModal");
+            this.$getNamedFieldWithinModal($modal, "HostName").val("");
+            this.$getNamedFieldWithinModal($modal, "GroupHeader").val("");
+            this.$getNamedFieldWithinModal($modal, "SortValue").val("");
+            this.$getNamedFieldWithinModal($modal, "PageLocation").val("");
+            this.$getNamedFieldWithinModal($modal, "ContentItemNames").val("");
+            $modal.foundation('reveal', 'open');
+        };
+
+        DynamicContentViewController.prototype.EditDynamicContentGroup = function ($source) {
+            var $modal = this.$getNamedFieldWithin("EditDynamicContentGroupModal");
+            var me = this;
+            var jq = $;
+            var wnd = window;
+            var clickedEditID = $source.attr("data-objectid");
+            $.getJSON('../../AaltoGlobalImpact.OIP/DynamicContentGroup/' + clickedEditID + ".json", function (contentData) {
+                var currentObject = contentData;
+                var currentID = currentObject.ID;
+                var currentETag = currentObject.MasterETag;
+                var currentRelativeLocation = currentObject.RelativeLocation;
+                me.$getNamedFieldWithinModal($modal, "ID").val(currentID);
+                me.$getNamedFieldWithinModal($modal, "ETag").val(currentETag);
+                me.$getNamedFieldWithinModal($modal, "RelativeLocation").val(currentRelativeLocation);
+                me.$getNamedFieldWithinModal($modal, "HostName").val(contentData.HostName);
+                me.$getNamedFieldWithinModal($modal, "GroupHeader").val(contentData.GroupHeader);
+                me.$getNamedFieldWithinModal($modal, "SortValue").val(contentData.SortValue);
+                me.$getNamedFieldWithinModal($modal, "PageLocation").val(contentData.PageLocation);
+                me.$getNamedFieldWithinModal($modal, "ContentItemNames").val(contentData.ContentItemNames);
+                $modal.foundation('reveal', 'open');
+            });
+        };
+
         DynamicContentViewController.prototype.OpenAddDynamicContentModal = function () {
             var $modal = this.$getNamedFieldWithin("AddNewDynamicContentModal");
             this.$getNamedFieldWithinModal($modal, "HostName").val("");
             this.$getNamedFieldWithinModal($modal, "ContentName").val("");
             this.$getNamedFieldWithinModal($modal, "ElementQuery").val("");
+            this.$getNamedFieldWithinModal($modal, "EditType").val("");
+            this.$getNamedFieldWithinModal($modal, "PageLocation").val("");
             this.$getNamedFieldWithinModal($modal, "Description").val("");
             this.$getNamedFieldWithinModal($modal, "RawContent").val("");
             this.$getNamedFieldWithinModal($modal, "Content").val("");
@@ -89,6 +126,8 @@ define(["require", "exports", "../ViewControllerBase"], function(require, export
                 me.$getNamedFieldWithinModal($modal, "HostName").val(contentData.HostName);
                 me.$getNamedFieldWithinModal($modal, "ContentName").val(contentData.ContentName);
                 me.$getNamedFieldWithinModal($modal, "ElementQuery").val(contentData.ElementQuery);
+                me.$getNamedFieldWithinModal($modal, "EditType").val(contentData.EditType);
+                me.$getNamedFieldWithinModal($modal, "PageLocation").val(contentData.PageLocation);
                 me.$getNamedFieldWithinModal($modal, "Description").val(contentData.Description);
                 me.$getNamedFieldWithinModal($modal, "RawContent").val(contentData.RawContent);
                 me.$getNamedFieldWithinModal($modal, "ApplyActively").prop("checked", contentData.ApplyActively);
@@ -116,6 +155,8 @@ define(["require", "exports", "../ViewControllerBase"], function(require, export
                 me.$getNamedFieldWithinModal($modal, "HostName").html(contentData.HostName);
                 me.$getNamedFieldWithinModal($modal, "ContentName").html(contentData.ContentName);
                 me.$getNamedFieldWithinModal($modal, "ElementQuery").html(contentData.ElementQuery);
+                me.$getNamedFieldWithinModal($modal, "EditType").html(contentData.EditType);
+                me.$getNamedFieldWithinModal($modal, "PageLocation").html(contentData.PageLocation);
                 me.$getNamedFieldWithinModal($modal, "Description").html(contentData.Description);
                 me.$getNamedFieldWithinModal($modal, "ApplyActively").html(contentData.ApplyActively);
                 if (contentData.RawContent)
@@ -126,10 +167,69 @@ define(["require", "exports", "../ViewControllerBase"], function(require, export
             });
         };
 
+        DynamicContentViewController.prototype.Modal_SaveNewDynamicContentGroup = function ($modal) {
+            var hostName = this.$getNamedFieldWithinModal($modal, "HostName").val();
+            var groupHeader = this.$getNamedFieldWithinModal($modal, "GroupHeader").val();
+            var sortValue = this.$getNamedFieldWithinModal($modal, "SortValue").val();
+            var pageLocation = this.$getNamedFieldWithinModal($modal, "PageLocation").val();
+            var contentItemNames = this.$getNamedFieldWithinModal($modal, "ContentItemNames").val();
+
+            var saveData = {
+                HostName: hostName,
+                GroupHeader: groupHeader,
+                SortValue: sortValue,
+                PageLocation: pageLocation,
+                ContentItemNames: contentItemNames
+            };
+
+            var me = this;
+            var jq = $;
+            jq.blockUI({ message: '<h2>Adding new dynamic content group...</h2>' });
+            me.currOPM.CreateObjectAjax("AaltoGlobalImpact.OIP", "DynamicContentGroup", saveData, function () {
+                setTimeout(function () {
+                    jq.unblockUI();
+                    $modal.foundation('reveal', 'close');
+                    me.ReInitialize();
+                }, 2500);
+            }, me.CommonErrorHandler);
+        };
+
+        DynamicContentViewController.prototype.Modal_SaveExistingDynamicContentGroup = function ($modal) {
+            var id = this.$getNamedFieldWithinModal($modal, "ID").val();
+            var etag = this.$getNamedFieldWithinModal($modal, "ETag").val();
+            var objectRelativeLocation = this.$getNamedFieldWithinModal($modal, "RelativeLocation").val();
+            var hostName = this.$getNamedFieldWithinModal($modal, "HostName").val();
+            var groupHeader = this.$getNamedFieldWithinModal($modal, "GroupHeader").val();
+            var sortValue = this.$getNamedFieldWithinModal($modal, "SortValue").val();
+            var pageLocation = this.$getNamedFieldWithinModal($modal, "PageLocation").val();
+            var contentItemNames = this.$getNamedFieldWithinModal($modal, "ContentItemNames").val();
+
+            var saveData = {
+                HostName: hostName,
+                GroupHeader: groupHeader,
+                SortValue: sortValue,
+                PageLocation: pageLocation,
+                ContentItemNames: contentItemNames
+            };
+
+            var me = this;
+            var jq = $;
+            jq.blockUI({ message: '<h2>Saving Dynamic Content Group...</h2>' });
+            me.currOPM.SaveIndependentObject(id, objectRelativeLocation, etag, saveData, function () {
+                setTimeout(function () {
+                    jq.unblockUI();
+                    $modal.foundation('reveal', 'close');
+                    me.ReInitialize();
+                }, 2500);
+            }, me.CommonErrorHandler);
+        };
+
         DynamicContentViewController.prototype.Modal_SaveNewDynamicContent = function ($modal) {
             var hostName = this.$getNamedFieldWithinModal($modal, "HostName").val();
             var contentName = this.$getNamedFieldWithinModal($modal, "ContentName").val();
             var elementQuery = this.$getNamedFieldWithinModal($modal, "ElementQuery").val();
+            var editType = this.$getNamedFieldWithinModal($modal, "EditType").val();
+            var pageLocation = this.$getNamedFieldWithinModal($modal, "PageLocation").val();
             var description = this.$getNamedFieldWithinModal($modal, "Description").val();
             var rawContent = this.$getNamedFieldWithinModal($modal, "RawContent").val();
             rawContent = $("<div/>").text(rawContent).html();
@@ -141,6 +241,8 @@ define(["require", "exports", "../ViewControllerBase"], function(require, export
                 HostName: hostName,
                 ContentName: contentName,
                 ElementQuery: elementQuery,
+                EditType: editType,
+                PageLocation: pageLocation,
                 Description: description,
                 "ENC.RawContent": rawContent,
                 "ENC.Content": content,
@@ -166,6 +268,8 @@ define(["require", "exports", "../ViewControllerBase"], function(require, export
             var hostName = this.$getNamedFieldWithinModal($modal, "HostName").val();
             var contentName = this.$getNamedFieldWithinModal($modal, "ContentName").val();
             var elementQuery = this.$getNamedFieldWithinModal($modal, "ElementQuery").val();
+            var editType = this.$getNamedFieldWithinModal($modal, "EditType").val();
+            var pageLocation = this.$getNamedFieldWithinModal($modal, "PageLocation").val();
             var description = this.$getNamedFieldWithinModal($modal, "Description").val();
             var rawContent = this.$getNamedFieldWithinModal($modal, "RawContent").val();
             rawContent = $("<div/>").text(rawContent).html();
@@ -177,6 +281,8 @@ define(["require", "exports", "../ViewControllerBase"], function(require, export
                 HostName: hostName,
                 ContentName: contentName,
                 ElementQuery: elementQuery,
+                EditType: editType,
+                PageLocation: pageLocation,
                 Description: description,
                 "ENC.RawContent": rawContent,
                 "ENC.Content": content,
