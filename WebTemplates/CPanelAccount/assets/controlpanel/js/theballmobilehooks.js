546cfaa94ec8b321c4ce3d46713ed39fd03f68d0
Kalle Launiala <kalle.launiala@protonit.net>
2015-08-28 22:00:20 +0300

    Initial support for mobile hybrid ajax overrides

---
+++ WebTemplates/CPanelAccount/assets/controlpanel/js/theballmobilehooks.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Kalle on 28.8.2015.
+ */
+
+if($) {
+    $.ajaxSetup({
+        beforeSend: function (x, settings) {
+            if (JS2MobileBridge) {
+                if (settings.type == "GET") {
+                    if (x && x.overrideMimeType) {
+                        x.overrideMimeType("application/json;charset=UTF-8");
+                    }
+                    return true;
+                }
+                var result = JS2MobileBridge.ExecuteAjaxOperation(settings.url, settings.data);
+                if (result && settings.success) {
+                    var data = JSON.parse(result);
+                    settings.success(data.OperationResult);
+                }
+                return false;
+            }
+        }
+    });
+}
